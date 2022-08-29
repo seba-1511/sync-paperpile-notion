d65cd367fa2d6b0df57db253813eb0857c26d39b
seba-1511 <seba-1511@hotmail.com>
2022-08-28 19:09:35 -0700

    Public release.

---
+++ README.md
@@ -0,0 +1,56 @@
+# sync-paperpile-to-notion
+
+When a new paper is added to paperpile, add it to a notion library.
+
+## Installation
+
+### On Notion
+
+1. Create a new database (e.g. "Papers") with the columns named exactly:
+
+    1. `Title` of type title.
+    2. `Authors` of type text.
+    3. `Year` of type text.
+    4. `Link` of type url.
+    5. `Reference ID` of type text.
+
+2. Get the **database identifier** from the database page. If your database url is:
+
+    ```
+    https://www.notion.so/my_workspace/aaaabbbbccccddddeeeeffffgggghhhh
+    ```
+
+    Then the database identifier is: `aaaabbbbccccddddeeeeffffgggghhhh`.
+
+3. Create a new integration on [https://www.notion.so/my-integrations/](https://www.notion.so/my-integrations/).
+
+    1. Name: Paperpile to Notion
+    2. Associated Workspace: Workspace of the database.
+    3. Content Capabilities: Read Content, Update Content, Insert Content.
+    4. User Capabilities: Read user information, including email addresses.
+    5. Press "Submit" and copy the **Internal Integration Token**.
+
+4. On the database page, click "Share" (top right) and add "Paperpile to Notion" with edit access.
+
+### On GitHub
+
+1. Fork this repository with the green "Use this template" button.
+2. On you fork, go to: "Settings -> Secrets -> Actions".
+3. Create 2 new repository secrets named exactly:
+    
+    1. `NOTION_TOKEN`: Your integration's internal integration token, from step 3.5 above.
+    2. `DATABASE_IDENTIFIER`: Your database identifier, from step 2 above.
+
+
+### On Paperpile
+
+1. Click on the top-right gear, go to "Workflows and Integrations".
+2. Follow the instructions to add a new "BibTex Export", choosing:
+
+    1. Your GitHub repository fork as the repository.
+    2. `references.bib` as the export path.
+
+The first sync should start as soon as the Paperpile workflow is created, and subsequent syncs are triggered whenever papers are added or updated in your Paperpile.
+
+**Note**
+The first sync might take some time as Notion limits the API rate to ~ 3 requests / second; so if you have 1,000 papers it'll take ~ 6 minutes before they are all available in Notion.
---
+++ sync.py
@@ -0,0 +1,268 @@
+
+import bibtexparser
+import json
+import os
+import pickle
+import pprint
+import requests
+
+"""
+TODO:
+- Default icon for papers.
+"""
+
+ARCHIVE_PATH = 'archive.pk'
+BIB_PATH = 'references.bib'
+NOTION_TOKEN = os.environ['NOTION_TOKEN']
+DATABASE_IDENTIFIER = os.environ['DATABASE_IDENTIFIER']
+
+
+def notion_add_entry(
+    title='',
+    authors='',
+    year='0',
+    ref_id='',
+    link='',
+):
+    url = "https://api.notion.com/v1/pages"
+    payload = {
+        "parent": {
+            'database_id': DATABASE_IDENTIFIER,
+        },
+        "properties": {
+            'Title': {
+                'title': [{
+                    'text': {
+                        'content': title,
+                        }
+                    }]
+                },
+            'Authors': {
+                "rich_text": [{
+                    "type": "text",
+                    "text": {
+                        "content": authors,
+                    }
+                }],
+            },
+            'Year': {
+                "rich_text": [{
+                    "type": "text",
+                    "text": {
+                        "content": year,
+                    }
+                }],
+            },
+            'Reference ID': {
+                "rich_text": [{
+                    "type": "text",
+                    "text": {
+                        "content": ref_id,
+                    }
+                }],
+            },
+            'Link': {
+                "url": link,
+            },
+                    
+                    
+        },
+    }
+    #  pprint.pprint(payload)
+    headers = {
+        "Accept": "application/json",
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {NOTION_TOKEN}"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    #  pprint.pprint(json.loads(response.text))
+
+
+def notion_update_page(
+    page_id,
+    title='',
+    authors='',
+    year='0',
+    ref_id='',
+    link='',
+):
+    url = f"https://api.notion.com/v1/pages/{page_id}"
+    payload = {
+        "parent": {
+            'database_id': DATABASE_IDENTIFIER,
+        },
+        "properties": {
+            'Title': {
+                'title': [{
+                    'text': {
+                        'content': title,
+                        }
+                    }]
+                },
+            'Authors': {
+                "rich_text": [{
+                    "type": "text",
+                    "text": {
+                        "content": authors,
+                    }
+                }],
+            },
+            'Year': {
+                "rich_text": [{
+                    "type": "text",
+                    "text": {
+                        "content": year,
+                    }
+                }],
+            },
+            'Reference ID': {
+                "rich_text": [{
+                    "type": "text",
+                    "text": {
+                        "content": ref_id,
+                    }
+                }],
+            },
+            'Link': {
+                "url": link,
+            },
+                    
+                    
+        },
+    }
+    #  pprint.pprint(payload)
+    headers = {
+        "Accept": "application/json",
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {NOTION_TOKEN}"
+    }
+    response = requests.patch(url, json=payload, headers=headers)
+
+
+def notion_fetch_page(ref_id):
+    url = f"https://api.notion.com/v1/databases/{DATABASE_IDENTIFIER}/query"
+
+    # list database pages
+    payload = {
+        "page_size": 1,
+        "filter": {
+            'property': 'Reference ID',
+            'rich_text': {"equals": ref_id},
+        },
+    }
+    headers = {
+        "Accept": "application/json",
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {NOTION_TOKEN}"
+    }
+    
+    response = requests.post(url, json=payload, headers=headers)
+    
+    response = json.loads(response.text)
+    #  pprint.pprint(response)
+    try:
+        if len(response['results']) > 0:
+            return response['results'][0]['id']
+    except:
+        return -1
+    return -1
+
+
+def clean_str(string):
+    string = string.strip()
+    string = string.replace('\n', ' ')
+    string = string.replace(r'\"a', 'ä')
+    string = string.replace(r'\"e', 'ë')
+    string = string.replace(r'\"i', 'ï')
+    string = string.replace(r'\"o', 'ö')
+    string = string.replace(r'\"u', 'ü')
+    string = string.replace(r'\'a', 'á')
+    string = string.replace(r'\'e', 'é')
+    string = string.replace(r'\'i', 'í')
+    string = string.replace(r'\'o', 'ó')
+    string = string.replace(r'\'u', 'ú')
+    string = string.replace(r'\^a', 'â')
+    string = string.replace(r'\^e', 'ê')
+    string = string.replace(r'\^i', 'î')
+    string = string.replace(r'\^o', 'ô')
+    string = string.replace(r'\^u', 'û')
+    string = string.replace(r'\`a', 'à')
+    string = string.replace(r'\`e', 'è')
+    string = string.replace(r'\`i', 'ì')
+    string = string.replace(r'\`o', 'ò')
+    string = string.replace(r'\`u', 'ù')
+    string = ' '.join([w.title() if w.islower() else w for w in string.split()])
+    string = string.replace('{', '')
+    string = string.replace('}', '')
+    return string
+
+
+def main():
+
+    # instantiate the parser
+    parser = bibtexparser.bparser.BibTexParser()
+    parser.ignore_nonstandard_types = True
+    parser.homogenize_fields = False
+    parser.interpolate_strings = False
+
+    with open(BIB_PATH) as bib_file:
+        bibliography = bibtexparser.load(bib_file, parser=parser)
+
+    if os.path.exists(ARCHIVE_PATH):
+        with open(ARCHIVE_PATH, 'rb') as archive_file:
+            archive = pickle.load(archive_file)
+    else:
+        archive = []
+    archive_ids = [e['ID'] for e in archive]
+
+    # add each entry to notion database
+    update_archive = False
+    for entry in bibliography.entries:
+
+        title = entry.get('title', '')
+        title = clean_str(title)
+        title = title
+
+        authors = entry.get('author', '')
+        authors = authors.replace(' and ', '; ')
+        authors = clean_str(authors)
+
+        year = entry.get('year', '')
+        link = entry.get('url', '')
+        ref_id = entry.get('ID')
+
+        notion_fetch_page(ref_id)
+
+        if ref_id not in archive_ids: # new page
+            notion_add_entry(
+                title=title,
+                authors=authors,
+                year=year,
+                ref_id=ref_id,
+                link=link,
+            )
+            update_archive = True
+        elif entry not in archive: # update existing page
+            page_id = notion_fetch_page(ref_id)
+            if page_id != -1:
+                notion_update_page(
+                    page_id=page_id,
+                    title=title,
+                    authors=authors,
+                    year=year,
+                    ref_id=ref_id,
+                    link=link,
+                )
+                update_archive = True
+
+    # only update the archive if necessary
+    if update_archive:
+        with open(ARCHIVE_PATH, 'wb') as archive_file:
+            archive = pickle.dump(bibliography.entries, archive_file)
+
+
+if __name__ == "__main__":
+    main()
